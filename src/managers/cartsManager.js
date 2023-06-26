7d864b23925b95828212f81941ba5a754d6073e2
santiago maza <mazasantiago.10@gmail.com>
2023-06-26 20:10:16 -0300

    realizando carts manager

---
+++ src/managers/cartsManager.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import { __dirname } from "../utils.js";
+
+const pathFile = __dirname + "/db/carts.json";
+
+export const getAllCarts = async () => {
+  try {
+    if (fs.readFileSync(pathFile)) {
+      const carts = await fs.promises.readFile(pathFile, "utf-8");
+      const cartsJSON = JSON.parse(carts);
+      return console.log(cartsJSON);
+    } else {
+      return console.log([]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getAllCarts()
